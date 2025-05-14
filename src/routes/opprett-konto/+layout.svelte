2076c05d7efcc3ab3db352de9cfda7414b99f115
Shahrukh Khan <shahrukh.khan@bouvet.no>
2025-05-14 15:54:16 +0200

    Add address page

--- src/routes/opprett-konto/+layout.svelte
+++ src/routes/opprett-konto/+layout.svelte
@@ -6,11 +6,11 @@
 
 <main class="max-h-screen h-screen">
     <div class="h-full flex flex-col items-center">
-    <div class="grid grid-rows-3 h-full max-w-md ">
+    <div class="grid grid-rows-[repeat(3, minmax(0, 0.2fr))] sm:grid-rows-3 max-h-full h-full max-w-md ">
         <div class="flex flex-col items-center row-start-1 mt-10 mx-10 md:mx-0">
             <ReferalPill name="Shahrukh" />
         </div>
 
-    {@render children()}
+        {@render children()}
     </div>
 </main>
---
+++ src/routes/opprett-konto/legg-til-adresser/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { ArrowRight, Info } from '@lucide/svelte/icons';
+</script>
+
+
+<div class="flex flex-col row-start-2 space-y-6 m-10 md:mx-0 text-left pb-20">
+    <img src="/Electric-house.png" alt="House" class="w-[144px] h-[121px] self-center" />
+    <h1 class="text-3xl ">Legg til dine adresser</h1>
+    <p>Logg inn med BankID, så henter vi adressene dine og strømdata fra Elhub – Statnetts dataportal for strøm.</p>
+    <div class="flex items-center space-x-2">
+        <Info class="text-secondary-500" />
+        <p>Velg alternativet «<span class="font-bold"> Strømforbruk og informasjon om strømleverandør</span>»</p>   
+    </div>
+    <button class="flex items-center justify-between px-4 py-3 bg-secondary-500 rounded-full w-full text-white">
+        <span class="flex-1 text-lg"> Hent min strømdata</span>
+        <span class="ml-auto"> <ArrowRight class="size-7" /> </span>
+    </button>
+</div>
+<div class="row-start-3"></div>
+
