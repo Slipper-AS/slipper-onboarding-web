4db83e6fea2f406f87d0454d7c541aa82a6e7cd6
Shahrukh Khan <shahrukh.khan@bouvet.no>
2025-06-12 14:01:37 +0200

    Add GA4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 
+	import { afterNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
 	let { children } = $props();
+	onMount(() => {
+		// Send pageview on initial load
+		gtag('event', 'page_view', { page_path: location.pathname });
+	});
+
+	afterNavigate(() => {
+		gtag('event', 'page_view', { page_path: location.pathname });
+	});
 </script>
 
 {@render children()}
