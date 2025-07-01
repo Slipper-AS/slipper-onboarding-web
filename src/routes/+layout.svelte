ad6ca913df6278b2e7a7eadf05ba3148268c1f91
Shahrukh Khan <shahrukh.khan@bouvet.no>
2025-07-01 10:17:23 +0200

    Fix consent for rudderstack tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,23 +4,28 @@
 	import { afterNavigate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import CookieBanner from '$lib/components/CookieBanner.svelte';
-	import { env } from '$env/dynamic/public';
 
 	let { children } = $props();
-	let rudderanalytics;
 
 	onMount(async () => {
 		// Send pageview on initial load
 		gtag('event', 'page_view', { page_path: location.pathname });
 
-		// Initialize RudderStack
-		const module = await import('rudder-sdk-js');
-		rudderanalytics = module;
-		if (env.PUBLIC_WRITE_KEY_RUDDERSTACK && env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK) {
-			rudderanalytics.load(env.PUBLIC_WRITE_KEY_RUDDERSTACK, env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK);
-			rudderanalytics.page('Onboarding Loaded'); // Track initial page view
-		} else {
-			console.warn('RudderStack write key is not defined');
+		if (localStorage.getItem('cookie_consent') === 'granted') {
+			// Initialize RudderStack
+			const rudderanalytics = await import('rudder-sdk-js');
+			if (
+				import.meta.env.PUBLIC_WRITE_KEY_RUDDERSTACK &&
+				import.meta.env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK
+			) {
+				rudderanalytics.load(
+					import.meta.env.PUBLIC_WRITE_KEY_RUDDERSTACK,
+					import.meta.env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK
+				);
+				rudderanalytics.page('Onboarding Loaded'); // Track initial page view
+			} else {
+				console.warn('RudderStack write key is not defined');
+			}
 		}
 	});
 
