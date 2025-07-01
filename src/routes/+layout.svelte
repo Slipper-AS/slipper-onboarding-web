5fbbc24ac8b7f9f5f40951121857dfd874061b65
Shahrukh Khan <shahrukh.khan@bouvet.no>
2025-07-01 09:36:28 +0200

    Remove meta pixel and add rudderstack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,21 @@
 	import { afterNavigate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import CookieBanner from '$lib/components/CookieBanner.svelte';
+	import * as rudderanalytics from 'rudder-sdk-js';
+	import { env } from '$env/dynamic/private';
 
 	let { children } = $props();
 	onMount(() => {
 		// Send pageview on initial load
 		gtag('event', 'page_view', { page_path: location.pathname });
+
+		// Initialize RudderStack
+		if (env.PUBLIC_WRITE_KEY_RUDDERSTACK && env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK) {
+			rudderanalytics.load(env.PUBLIC_WRITE_KEY_RUDDERSTACK, env.PUBLIC_DATA_PLANE_URI_RUDDERSTACK);
+			rudderanalytics.page('Onboarding Loaded'); // Track initial page view
+		} else {
+			console.warn('RudderStack write key is not defined');
+		}
 	});
 
 	afterNavigate(() => {
